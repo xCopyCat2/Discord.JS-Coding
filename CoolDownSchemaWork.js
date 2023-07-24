96f424aa1c59718ffff491975bf26922be72532b
xCopyCat2 <140447150+xCopyCat2@users.noreply.github.com>
2023-07-24 22:46:20 +0300

    Delete CoolDownSchemaWork.js

--- CoolDownSchemaWork.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const cooldownSchemaWork = new mongoose.Schema({
-    UserId: { type: String, required: true },
-    LastUsed: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('CooldownWork', cooldownSchemaWork);
