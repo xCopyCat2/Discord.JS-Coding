85e63c34297c0202fa86df77454ca382a5459902
xCopyCat2 <140447150+xCopyCat2@users.noreply.github.com>
2023-07-24 22:47:38 +0300

    Delete CoolDownSchemaRob.js

--- CoolDownSchemaRob.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const cooldownSchemaRob = new mongoose.Schema({
-    UserId: { type: String, required: true },
-    LastUsed: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('CooldownRob', cooldownSchemaRob);
