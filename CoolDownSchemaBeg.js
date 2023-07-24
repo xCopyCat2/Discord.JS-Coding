ce61379357ee01c8fe0acbed1f52da64b8f97f8b
xCopyCat2 <140447150+xCopyCat2@users.noreply.github.com>
2023-07-24 22:47:15 +0300

    Delete CoolDownSchemaBeg.js

--- CoolDownSchemaBeg.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const cooldownSchemaBeg = new mongoose.Schema({
-    UserId: { type: String, required: true },
-    LastUsed: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('CooldownBeg', cooldownSchemaBeg);
